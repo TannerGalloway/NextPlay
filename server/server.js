657011ca3843fab7cbddd2829b071b16a477e5dd
TannerGalloway <tannergalloway15@gmail.com>
2025-02-19 01:07:29 +0900

    feat: Server Setup and Route Configuring

--- server/server.js
+++ server/server.js
@@ -1,10 +1,30 @@
-require("dotenv").config();
-const express = require("express");
+import "@dotenvx/dotenvx/config";
+import express from "express";
+import authRoutes from "./routes/authRoutes.js";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Middleware for parsing JSON
 app.use(express.json());
 
+// Get the environment variable to check if it's development or production
+const isProduction = process.env.NODE_ENV === 'production';
 
-app.listen(PORT, () => { console.log(`Server started on port ${PORT}`)});
+if(!isProduction) {
+    // Middleware for CORS to accect requests from the client
+    app.use(cors({
+        origin: ["http://localhost:5173"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true,
+    }));
+
+}
+
+// API Routes
+app.use("/auth", authRoutes);
+
+
+app.listen(PORT, () => { console.log(`Server started on port ${PORT}`) });
