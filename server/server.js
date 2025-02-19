469e6515a6e0a4c3d193eb5f7f146e8f240b2dbe
TannerGalloway <tannergalloway15@gmail.com>
2025-02-19 18:48:48 +0900

    fix: Send static files to front end

--- server/server.js
+++ server/server.js
@@ -35,6 +35,10 @@ if (isProduction) {
 
     // Display the files built from vite
     app.use(express.static(path.join("../client/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join("../client/dist", "index.html"));
+      });
 }
 
 // Auth Routes
